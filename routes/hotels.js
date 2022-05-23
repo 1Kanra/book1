bef1c4d4e5d6e7cd4b6fa9ad7e124c6626c5d05a
1Kanra <106079194+1Kanra@users.noreply.github.com>
2022-05-23 19:52:05 +0800

    Add files via upload
    read readme

---
+++ routes/hotels.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+
+const router = express.Router();
+
+//create
+router.post("/", createHotel);
+//update 
+router.put("/:id", updateHotel);
+//delete
+router.delete("/:id", deleteHotel);
+//get
+router.get("/:id", getHotel);
+//getall
+router.get("/", getHotels);
+
+export default router;
